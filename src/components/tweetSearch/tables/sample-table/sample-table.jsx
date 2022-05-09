b5faf8e948850f8e8de4f904a4eaf77dfb6d0e4c
pratikjoil999 <pratikjoil999@gmail.com>
2022-05-09 21:48:00 +0530

    latest

--- src/components/tweetSearch/tables/sample-table/sample-table.jsx
+++ src/components/tweetSearch/tables/sample-table/sample-table.jsx
@@ -13,17 +13,8 @@ export default class SampleTable extends React.Component {
     };
   }
 
-  componentDidMount() {
-    this.getPaitent();
-  }
+  componentDidMount() {}
 
-  getPaitent = () => {
-    getDataService("/patient-details-find-all").then((result) => {
-      let responseJSON = result;
-      this.setState({ patent: responseJSON.patientDetailsList });
-      console.log(this.state.patent);
-    });
-  };
   render() {
     return (
       <div className="sampleTable">
@@ -35,20 +26,24 @@ export default class SampleTable extends React.Component {
                   <div class="tweet-dp col-2 d-block">
                     <div class="dp-letter mt-3">
                       <div class="dp mt-2">
-                        {tweet.userData.full_name.toUpperCase().substring(0, 1)}
+                        { tweet.userData.full_name.toUpperCase().substring(0, 1)}
                       </div>
                     </div>
                   </div>
                   <div class="col-10 p-0">
                     <div class="tweet-text-div col-12 p-0 mt-3">
-                      <div className="user_name">
-                        {tweet.userData.first_name +
-                          " " +
-                          tweet.userData.last_name}{" "}
-                        {tweet.userData.user_name}
+                      <div className="user_name d-flex">
+                        <div className="profile_name">
+                          {tweet.userData.first_name +
+                            " " +
+                            tweet.userData.last_name}
+                        </div>{" "}
+                        <div className="user_name_text ml-2">
+                          {"@" + tweet.userData.user_name}
+                        </div>
                       </div>
                     </div>
-                    <div class="w80">
+                    <div class="w80 mt-2">
                       <div class="tweet-button-div col-12 p-0">
                         <Hashtags>{tweet.tweet}</Hashtags>
                       </div>
